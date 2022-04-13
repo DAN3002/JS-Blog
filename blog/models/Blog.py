f7fb5551c96928e152c041a774f3d0a099ee0177
D.A.N_3002 <dinhanh300229@gmail.com>
2022-04-13 15:51:50 +0700

    [ADD] Add view blog

--- blog/models/Blog.py
+++ blog/models/Blog.py
@@ -6,6 +6,31 @@ class Blog(db.Model):
 	content = db.Column(db.String)
 	owner = db.Column(db.String)
 	role = db.Column(db.String)
-	date = db.Column(db.DateTime)
 
+class BlogController():
+	@staticmethod
+	def get_all_blogs():
+		return Blog.query.all()
 
+	@staticmethod
+	def get_blog_by_id(id):
+		return Blog.query.filter_by(id=id).first()
+
+	@staticmethod
+	def get_blog_by_owner(owner):
+		return Blog.query.filter_by(owner=owner).all()
+
+	@staticmethod
+	def get_blog_by_role(role):
+		return Blog.query.filter_by(role=role).all()
+
+	@staticmethod
+	def get_blog_by_date(date):
+		return Blog.query.filter_by(date=date).all()
+
+	@staticmethod
+	def create_blog(content, owner, role):
+		blog = Blog(content=content, owner=owner, role=role, date=datetime.now())
+		db.session.add(blog)
+		db.session.commit()
+		return blog
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,16 @@
-from flask import Blueprint
-from flask import render_template
+from flask import Blueprint, render_template, request, redirect, url_for
+from ..blog.models.Blog import BlogController
 
 blog = Blueprint('blog', __name__)
 
-@blog.route('/')
-def index():	
-	return render_template('blog.html')
+@blog.route('/view')
+def view_blog():
+	id = request.args.get('id', None)
+
+	if id is None:
+		return redirect(url_for('homepage.index'))
+
+	blog = BlogController.get_blog_by_id(id)
+	return render_template('blog.html', blog=blog)
+
+
